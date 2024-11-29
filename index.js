0002f86c29b65dc8d55eb4ca777fb6289ec35f52
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-29 12:46:54 +0100

    Use runtime detected conditional export

--- index.js
+++ index.js
@@ -1,50 +1,9 @@
-const url = require('url')
-const resolve = require('bare-addon-resolve')
-const { runtime, platform, arch } = require('./lib/runtime')
+const runtime = require('./lib/runtime')
 
-const host = platform + '-' + arch
-const conditions = [platform, arch]
-const extensions = []
-
-switch (runtime) {
-  case 'bare':
-    conditions.push('bare', 'node')
-    extensions.push('.bare', '.node')
-    break
-  case 'node':
-    conditions.push('node')
-    extensions.push('.node')
-    break
-}
-
-module.exports = function addon(specifier, parentURL) {
-  if (typeof parentURL === 'string') parentURL = url.pathToFileURL(parentURL)
-
-  for (const resolution of resolve(
-    specifier,
-    parentURL,
-    { host, conditions, extensions },
-    readPackage
-  )) {
-    switch (resolution.protocol) {
-      case 'file:':
-        try {
-          return require(url.fileURLToPath(resolution))
-        } catch {
-          continue
-        }
-    }
-  }
-
-  throw new Error(
-    `Cannot find addon '${specifier}' imported from '${parentURL.href}'`
-  )
-
-  function readPackage(packageURL) {
-    try {
-      return require(url.fileURLToPath(packageURL))
-    } catch (err) {
-      return null
-    }
-  }
+if (runtime === 'bare') {
+  module.exports = require('./lib/runtime/bare')
+} else if (runtime === 'node') {
+  module.exports = require('./lib/runtime/node')
+} else {
+  module.exports = require('./lib/runtime/default')
 }
